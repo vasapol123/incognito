c76639b456e4fcf0ac39651cd66f087e90d28ccd
vasapol123 <vasapol123@gmail.com>
2022-11-25 20:14:34 +0700

    fix: fix nest unable to resolve dependencies by adding missing imports

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '@incognito/prisma';
+import { UsersModule } from './users/users.module';
+import { TokensModule } from './tokens/tokens.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
+    UsersModule,
+    TokensModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/server/src/app/users/users.service.spec.ts
+++ apps/server/src/app/users/users.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from '../app.module';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -6,7 +8,7 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [AppModule],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
